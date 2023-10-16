3a39403196a8c1a18e937e2a0579784a466a1481
Stuart_Christhilf <stuartchristv@gmail.com>
2023-10-16 10:45:29 -0400

    basics of playerlist data sturcture

---
+++ PlayerList.js
@@ -0,0 +1,19 @@
+const Turtle = require('./turtleInterface');
+
+const PlayerList = {
+    dataStructure: [],
+    add: function(value) {
+        // Implementation for adding to an array
+    },
+    delete: function(index) {
+        // Implementation for deleting from an array
+    },
+    resize: function(newSize) {
+        // Implementation for resizing an array
+    },
+    check: function() { 
+
+    }
+};
+
+module.exports = PlayerList; // Export the object
