fc090505a6fd90e2ad7d0bc37e5b4809db8dcc64
Stuart_Christhilf <stuartchristv@gmail.com>
2023-10-16 10:41:33 -0400

    addign interface Turtle

---
+++ Turtle.js
@@ -0,0 +1,8 @@
+const Turtle = {
+    add: function() {},
+    delete: function() {},
+    resize: function() {}
+};
+
+// Export the interface to make it available to other files (CommonJS module style)
+module.exports = Turtle;
