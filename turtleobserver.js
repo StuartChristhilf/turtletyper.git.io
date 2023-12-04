4db80b64d1652979acf7b5f323ead689910f945a
StuartChristhilf <stuartchristv@gmail.com>
2023-12-04 08:52:23 -0500

    so i made is so in the observer it stops giving me the no next thing all i need to do is actulaluy updata the screen now.

--- turtleobserver.js
+++ turtleobserver.js
@@ -2,30 +2,57 @@
 import { PlayerList } from './PlayerList.js';
 
 export class TurtleObserver {//observe
+    
     constructor(turtleTree, characterDisplay, updateNodeDisplay) {
         this.turtleTree = turtleTree;//hhi
         this.characterDisplay = characterDisplay;
         this.updateNodeDisplay = updateNodeDisplay;
+        
     }
 
     checkMatchingCharacters() {
+
         // Get the characters from the PlayerList
         const playerCharacters = PlayerList.getAllCharacters();
     
         // Get the data of the current node from the TurtleTree
         const currentNodeData = this.turtleTree.getCurrentNodeData();
-        const cNode = this.turtleTree.getCurrentNode();
+        var cNode = this.turtleTree.getFirstNode();
         // Check if the typed characters exactly match the characters in the current node
-        const match = playerCharacters.join('') === currentNodeData;
+        const match = playerCharacters.join('') === cNode.data;
     
         // Update the character display
         this.characterDisplay.textContent = match ? 'MATCH' : 'NO MATCH';
         console.log("Player Characters:", playerCharacters);
-        console.log("Current Node Data:", currentNodeData);
+        console.log("Current Node Data:", cNode.data);
     
         if (match) {
+            
             console.log("same");
-            this.turtleTree.moveToNextNode(); // Move to the next node
+            if (x == undefined){
+                cNode = this.turtleTree.getFirstNode().right;
+                var x = 1;
+            } 
+            if(x == 1){
+                cNode = this.turtleTree.getFirstNode().right.right;
+                x++;
+            }
+            if(x == 2){
+                cNode = this.turtleTree.getFirstNode().right.right.right;
+                x++;
+            }
+            if(x == 3){
+                cNode = this.turtleTree.getFirstNode().right.right.right.right;
+                x++;
+            }
+            if(x == 4){
+                cNode = this.turtleTree.getFirstNode().right.right.right.right.right;
+                x++;
+            }
+            if(x == 5){
+                cNode = this.turtleTree.getFirstNode().right.right.right.right.right.right;
+                x++;
+            }
             this.updateNodeDisplay(); // Update the display after moving to the next node
         } else {
             console.log("different");
