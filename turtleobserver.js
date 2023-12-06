c021463cbf41e3f2371fb4d19707232a4b2bdbf6
StuartChristhilf <stuartchristv@gmail.com>
2023-12-06 08:22:46 -0500

    done thank god and all of his might goodness even though im atheist i have no other explantion for my codew works now so i prease the almight. halaluya. I changed it so now the turtle observer canm see its own mnode and they are linked so it all works. dhjafgsjyudashjfghjsadghjfdjhasgfhjd

--- turtleobserver.js
+++ turtleobserver.js
@@ -3,11 +3,11 @@ import { PlayerList } from './PlayerList.js';
 
 export class TurtleObserver {//observe
     
-    constructor(turtleTree, characterDisplay, updateNodeDisplay) {
+    constructor(turtleTree, characterDisplay, updateNodeDisplay, cNode) {
         this.turtleTree = turtleTree;//hhi
         this.characterDisplay = characterDisplay;
         this.updateNodeDisplay = updateNodeDisplay;
-        
+        this.cNode = cNode;
     }
 
     checkMatchingCharacters() {
@@ -17,21 +17,20 @@ export class TurtleObserver {//observe
     
         // Get the data of the current node from the TurtleTree
         const currentNodeData = this.turtleTree.getCurrentNodeData();
-        var cNode = this.turtleTree.getFirstNode();
         // Check if the typed characters exactly match the characters in the current node
-        const match = playerCharacters.join('') === cNode.data;
+        const match = playerCharacters.join('') === this.cNode.data;
     
         // Update the character display
         this.characterDisplay.textContent = match ? 'MATCH' : 'NO MATCH';
         console.log("Player Characters:", playerCharacters);
-        console.log("Current Node Data:", cNode.data);
+        console.log("Current Node Data:", this.cNode.data);
     
         if (match) {
             
             console.log("same");
-            console.log(cNode);
-            cNode = cNode.right;
-            this.updateNodeDisplay(cNode); // Update the display after moving to the next node
+            console.log(this.cNode);
+            this.cNode = this.cNode.right;
+            this.updateNodeDisplay(this.cNode); // Update the display after moving to the next node
         } else {
             console.log("different");
         }
